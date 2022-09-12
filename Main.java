09710be261e35537fcd90c5946aa177585a71f3b
Jussi Pohjolainen <jussi.pohjolainen@gmail.com>
2022-09-12 10:45:45 +0300

    Otsikon printtaus lisatty

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 class Main {
     public static void main(String [] args) {
+        System.out.println("Eka tehtavani tassa");
         for(int i=0; i<3; i++) {
             System.out.println(i);
         }
